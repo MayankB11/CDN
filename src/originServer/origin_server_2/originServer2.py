a3f5182704e4fd0f7c4778c863d8dd13734986d7
Animesh Kashyap <annimesh2809@gmail.com>
2019-04-18 00:14:55 +0530

    origin server fixed

--- src/originServer/origin_server_2/originServer2.py
+++ src/originServer/origin_server_2/originServer2.py
@@ -14,8 +14,9 @@ import selectors
 import os
 from enum import Enum
 import pickle
+from threading import Timer, Thread, Lock
 
-sys.path.insert(0, "../")
+sys.path.insert(0, "../../")
 
 from messages.content_related_messages import *
 from messages.origin_heartbeat_message import *
@@ -52,6 +53,12 @@ def load():
 	content_dict = pickle.load(f)
 	f.close()
 
+def print_dict():
+	global content_dict
+	for content in content_dict.values():
+		print(content.content_id, content.filename, content.status)
+
+
 ####################################
 
 def synchronizer():
@@ -61,7 +68,7 @@ def synchronizer():
 		sock = socket.socket()
 		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		host = socket.gethostname()
-		port = ORIGIN_SYNCHRONIZER_PORT
+		port = ORIGIN_SYNCHRONIZER_PORT_2
 		sock.bind((host, port))
 		sock.listen(1)
 		conn, addr = sock.accept()
@@ -70,17 +77,18 @@ def synchronizer():
 
 		while(True):
 			print("Looking for UNSYNCED files")
-			for file in content_dict.keys():
+			for file in content_dict.values():
 				if file.status == ContentStatus.UNSYNCED:
 					# Sync this file
 					print("Syncing file", file.filename, "with content id", file.content_id)
-					file_size = int(os.stat(file.filename).st_size)
-					file_des = FileDescriptionMessage(file.content_id,file_size,file.filename,md5(file.filename))
-					file_des.send(sock)
+					file_size = int(os.stat('data/'+file.filename).st_size)
+					file_des = FileDescriptionMessage(file.content_id,file_size,file.filename,md5('data/'+file.filename))
+					print(file.content_id,file_size,file.filename,md5('data/'+file.filename))
+					file_des.send(conn)
 
 					# receive response from other server
 					msg = OriginHeartbeatMessage(0)
-					msg.receive(sock)
+					msg.receive(conn)
 					if msg.file_exists:
 						content_dictL.acquire()
 						content_dict[file_des.content_id].status = ContentStatus.STORED
@@ -88,7 +96,7 @@ def synchronizer():
 						content_dictL.release()
 						continue
 
-					f = open(file.filename, 'rb')
+					f = open('data/'+file.filename, 'rb')
 					l = f.read(1018)
 					i = 0
 					while (l):
@@ -96,15 +104,15 @@ def synchronizer():
 						msg = ContentMessage(file.content_id, i)
 						msg.data = l
 						msg.packet_size = len(l)
-						msg.send(sock)
+						msg.send(conn)
 						i += 1
 						l = f.read(1018)
 					f.close()
 
-				content_dictL.acquire()
-				content_dict[file_des.content_id] = Content(file_des.content_id, file_des.file_name, ContentStatus.STORED)
-				dump()
-				content_dictL.release()
+					content_dictL.acquire()
+					content_dict[file_des.content_id].status = ContentStatus.STORED
+					dump()
+					content_dictL.release()
 			time.sleep(ORIGIN_HEARTBEAT_TIME)
 		
 def synchronize_receive():
@@ -118,7 +126,7 @@ def synchronize_receive():
 		print('Socket creation failed with error %s', err)
 		return
 	host = socket.gethostname()
-	port = ORIGIN_SYNCHRONIZER_PORT
+	port = ORIGIN_SYNCHRONIZER_PORT_1
 
 	while(True):
 
@@ -167,18 +175,18 @@ def synchronize_receive():
 				content_dict[file_des.content_id] = Content(file_des.content_id, file_des.file_name, ContentStatus.INCOMPLETE)
 				dump()
 				content_dictL.release()
-				with open('rec_' + file_des.file_name, 'wb') as f:
+				with open('data/' + file_des.file_name, 'wb') as f:
 					print('file opened')
 					print("Content ID: ",file_des.content_id)
-					if seq_no!=0:
-						f.seek(seq_no*1018)
+					content_id = file_des.content_id
 					file_size = file_des.file_size
 					total_received=0
+					seq_no=0
 					while True:
 						msg = ContentMessage(content_id, seq_no)
 
 						try:
-							msg.receive(soc,file_size,total_received)
+							msg.receive(sock,file_size,total_received)
 						except Exception as e:
 							print("Last Sequence Number received: ",last_seq_number_recv)
 							print(e)
@@ -243,7 +251,7 @@ def serve_edge_server():
 		print ("socket creation failed with error %s" %(err)) 
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	port = ORIGIN_SERVER_PORT
+	port = ORIGIN_SERVER_PORT_2
 	s.bind(('', port))         
 	print ("socket binded to %s" %(port)) 	
 	
@@ -272,18 +280,27 @@ def serve_content_provider_helper(c,addr):
 	content_dict[file_des.content_id] = Content(file_des.content_id, file_des.file_name, ContentStatus.INCOMPLETE)
 	dump()
 	content_dictL.release()
+
 	with open('data/'+file_des.file_name,'wb') as f:
+		recv_size = 0 
+		file_size = file_des.file_size
 		while True:
 			mes = ContentMessage(0,0)
 			print('receiving data...')
-			mes.receive(c)
+			
+			mes.receive(c,file_size,recv_size)
 			print(mes.content_id) 
 			print(mes.seq_no)
+			
 			data = mes.data
 			if not data:
 				break
+
 			f.write(data)
-		print("successfully received the file")
+			recv_size+=len(data)
+
+	print("successfully received the file")
+
 	if md5('data/'+file_des.file_name) == file_des.md5_val:
 		print("MD5 Matched!")
 	else:
@@ -292,6 +309,7 @@ def serve_content_provider_helper(c,addr):
 	content_dict[file_des.content_id].status = ContentStatus.UNSYNCED
 	dump()
 	content_dictL.release()
+	print_dict()
 	c.close()
 
 def serve_content_provider():
@@ -302,7 +320,7 @@ def serve_content_provider():
 		print ("socket creation failed with error %s" %(err)) 
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	port = ORIGIN_CONTENT_PROVIDER_PORT
+	port = ORIGIN_CONTENT_PROVIDER_PORT_2
 	s.bind(('', port))         
 	print ("socket binded to %s" %(port)) 	
 	
